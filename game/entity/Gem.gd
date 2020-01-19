e5c36958c126db8675958c8b6f23119ed377cc40
Aezart Rothenus <heroofhyla@gmail.com>
2020-01-19 10:43:18 -0700

    Fix move queuing

--- game/entity/Gem.gd
+++ game/entity/Gem.gd
@@ -1,16 +1,15 @@
-tool
 extends Area2D
 
 export var color = 0 #setget set_color, get_color
 export var clickable = true
 onready var target_pos = position
 onready var game = get_node("/root/Game")
-var move_dir_sec = 0.5
+var move_dir_sec = 0.25
 var move_dir_progress = 0.0
 var neighbors = {}
 var move_queue = []
 var wait_time = 0.0
-
+onready var prev_pos = position
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
@@ -20,15 +19,37 @@ func _ready():
 func set_color(col):
 	$Sprite.frame = col
 	color = col
+
+func time_until_ready():
+	if is_idle():
+		return 0
 	
+	return move_dir_sec - move_dir_progress
+
+func is_idle():
+	if target_pos == position and len(move_queue) == 0:
+		return true
+	return false
+
+func set_wait_time(delay):
+	wait_time = delay - (time_until_ready())
+		
 func get_color():
 	return color
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	$WaitTime.text = "%.2f" % wait_time
+	if is_idle():
+		$Idle.text = "Idle"
+	else:
+		$Idle.text = "Busy: %.2f" % time_until_ready()
+	
 	if wait_time > 0 and position == target_pos:
 		wait_time -= delta
 		return
-		
+	
+	if wait_time < 0:
+		wait_time = 0
 	if target_pos == position and len(move_queue) != 0:
 		target_pos = move_queue.pop_front()
 		move_dir_progress = 0.0
@@ -37,12 +58,13 @@ func _process(delta):
 		var progress_frac = move_dir_progress / move_dir_sec
 		if move_dir_progress >= move_dir_sec:
 			position = target_pos
+			prev_pos = position
 			move_dir_progress = 0
 		else:
-			var x_delta = target_pos.x - position.x
-			var y_delta = target_pos.y - position.y
-			position.x += x_delta * progress_frac
-			position.y += y_delta * progress_frac
+			var x_delta = target_pos.x - prev_pos.x
+			var y_delta = target_pos.y - prev_pos.y
+			position.x = prev_pos.x + x_delta * progress_frac
+			position.y = prev_pos.y + y_delta * progress_frac
 	elif move_dir_progress != 0.0:
 		move_dir_progress = 0.0
 
@@ -103,17 +125,14 @@ func rotate_neighbors():
 	if len(move_queue) > 0:
 		return
 	
-	var least_progress = move_dir_sec
+	var max_wait_time = 0
 	for neighbor in neighbors.values():
 		var node = neighbor["node"]
-		if node.target_pos == node.position:
-			continue
-		least_progress = min(node.move_dir_progress, least_progress)
-	
-	if least_progress < move_dir_sec:
-		for neighbor in neighbors.values():
-			var node = neighbor["node"]
-			node.wait_time = (move_dir_sec - least_progress) - node.move_dir_progress
+		max_wait_time = max(max_wait_time, node.time_until_ready())
+		
+	for neighbor in neighbors.values():
+		var node = neighbor["node"]
+		node.set_wait_time(max_wait_time)
 	
 	var nw_pos = neighbors["nw"]["node"].final_target_pos()
 	neighbors["nw"]["node"].start_move_to(neighbors["w"]["node"].final_target_pos())
